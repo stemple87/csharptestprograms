ed7140a066e9db5c22a292e4f7c31961ac847412
simon Michael <simonctemple@live.com>
2016-02-08 09:40:07 -0800

    make a constructor with private variables.

--- Cars.cs
+++ Cars.cs
@@ -3,14 +3,53 @@ using System.Collections.Generic;
 
 class Car
 {
-  public string MakeModel;
-  public int Price;
-  public int Miles;
+  private string _makeModel;
+  private int _price;
+  private int _miles;
 
+  public void SetPrice (int newPrice)
+  {
+    if (newPrice >= 0) {
+      _price = newPrice;
+    }
+    else
+    {
+      Console.WriteLine("Not valid");
+    }
+  }
+  public int GetPrice()
+  {
+    return _price;
+
+  }
+
+  public void SetMakeModel(string newMakeModel)
+  {
+    _makeModel = newMakeModel;
+  }
+  public string GetMakeModel()
+  {
+    return _makeModel;
+  }
+
+  public void SetMiles(int newMiles)
+  {
+    _miles = newMiles;
+  }
+  public int GetMiles()
+  {
+    return _miles;
+  }
+
+  public Car (string SetMakeModel, int SetMiles, int SetPrice = 10000){
+    _makeModel = SetMakeModel;
+    _price = SetPrice;
+    _miles = SetMiles;
+  }
 
   public bool WorthBuying(int maxPrice, int maxMiles)
   {
-    return (Price < maxPrice && Miles < maxMiles);
+    return (_price < maxPrice && _miles < maxMiles);
   }
 }
 
@@ -18,49 +57,48 @@ public class Program
 {
   public static void Main()
   {
-    Car porsche = new Car();
-    porsche.MakeModel = "2014 Porsche 911";
-    porsche.Price = 114991;
-    porsche.Miles = 7864;
+    Car porsche = new Car("2014 Porsche 911", 7864);
 
-    Car ford = new Car();
-    ford.MakeModel = "2011 Ford F450";
-    ford.Price = 55995;
-    ford.Miles = 14241;
+    Car ford = new Car("2011 FOrd F450", 14241);
 
-    Car lexus = new Car();
-    lexus.MakeModel = "2013 Lexus RX 350";
-    lexus.Price = 44700;
-    lexus.Miles = 20000;
+    Car lexus = new Car("2013 Lexus RX 350", 20000);
 
-    Car mercedes = new Car();
-    mercedes.MakeModel = "Mercedes Benz CLS550";
-    mercedes.Price = 39900;
-    mercedes.Miles = 37979;
+    Car mercedes = new Car("Mercedes Benz CLS550", 37979);
 
+    Car nissan = new Car("2011 Pathfinder", 20000, 35000);
+
+    Console.WriteLine("Our current selection");
     List<Car> Cars = new List<Car>() { porsche, ford, lexus, mercedes };
+    Cars.Add(nissan);
+
     foreach(Car automobile in Cars)
     {
-      Console.WriteLine(automobile.MakeModel);
+      Console.WriteLine(automobile.GetMakeModel());
+      Console.WriteLine(automobile.GetPrice());
     }
 
     Console.WriteLine("Enter maximum price: ");
     string stringMaxPrice = Console.ReadLine();
     int maxPrice = int.Parse(stringMaxPrice);
 
+    Console.WriteLine("Enter maximum miles: ");
+    string stringMaxMiles = Console.ReadLine();
+    int maxMiles = int.Parse(stringMaxMiles);
+
     List<Car> CarsMatchingSearch = new List<Car>();
 
     foreach (Car automobile in Cars)
     {
-      if (automobile.Price < maxPrice)
+      if (automobile.WorthBuying(maxPrice, maxMiles))
       {
         CarsMatchingSearch.Add(automobile);
       }
     }
 
+    Console.WriteLine("Results matching your search");
     foreach(Car automobile in CarsMatchingSearch)
     {
-      Console.WriteLine(automobile.MakeModel);
+      Console.WriteLine(automobile.GetMakeModel());
     }
   }
 }
